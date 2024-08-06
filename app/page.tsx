40f1f970ade7a6ee1605152ca0026cd3c1d53007
Ruan Lins <ruanblins@gmail.com>
2024-08-05 22:07:21 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+import { SearchIcon } from "lucide-react"
+import { Header } from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Ruan!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            className="rounded-xl"
+            src="/Banner pizza.png"
+            alt="banner-01"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
