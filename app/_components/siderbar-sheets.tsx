a87eaaaf7cbdef606f92ce404b9053b1e4de6d10
Ruan Lins <ruanblins@gmail.com>
2024-08-13 23:11:43 -0300

    feat: added logged info on header

--- app/_components/siderbar-sheets.tsx
+++ app/_components/siderbar-sheets.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
 import React from "react"
 import { quickSearchOptions } from "../_constants/search"
@@ -14,8 +16,13 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog"
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const handleLoginWithGoogleClick = () => signIn("google")
+const handleLogoutClick = () => signOut()
 
 export const SidebarSheets = () => {
+  const { data } = useSession()
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
@@ -23,35 +30,49 @@ export const SidebarSheets = () => {
       </SheetHeader>
 
       <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-        <h2 className="font-bold">Olá! Faça seu login!</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size="icon">
-              <LogInIcon />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%]">
-            <DialogHeader>
-              <DialogTitle>Faça login na plataforma</DialogTitle>
-              <DialogDescription>
-                Conecte-se usando sua conta do Google
-              </DialogDescription>
-            </DialogHeader>
-            <Button variant="outline" className="gap-1 font-bold">
-              <Image src="/google.svg" width={18} height={18} alt="Google" />
-              Goole
-            </Button>
-          </DialogContent>
-        </Dialog>
-
-        {/* <Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Avatar>
-
-        <div>
-          <p className="font-bold">John Doe</p>
-          <p className="text-xs">johndoe@example.com</p>
-        </div> */}
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+            <div>
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="font-bold">Olá! Faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <DialogHeader>
+                  <DialogTitle>Faça login na plataforma</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta do Google
+                  </DialogDescription>
+                </DialogHeader>
+                <Button
+                  onClick={handleLoginWithGoogleClick}
+                  variant="outline"
+                  className="gap-1 font-bold"
+                >
+                  <Image
+                    src="/google.svg"
+                    width={18}
+                    height={18}
+                    alt="Google"
+                  />
+                  Goole
+                </Button>
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
@@ -89,7 +110,11 @@ export const SidebarSheets = () => {
       </div>
 
       <div className="flex flex-col gap-2 py-5">
-        <Button variant="ghost" className="justify-start gap-2">
+        <Button
+          onClick={handleLogoutClick}
+          variant="ghost"
+          className="justify-start gap-2"
+        >
           <LogOutIcon size={18} />
           Sair da conta
         </Button>
