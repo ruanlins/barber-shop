77e9cd4903617dcda8c75019d681935997290a6b
Ruan Lins <ruanblins@gmail.com>
2024-08-18 20:37:42 -0300

    fix: show exit account option only when a user is logged in

---
+++ app/_components/sign-in-dialog.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { DialogTitle, DialogDescription, DialogHeader } from "./ui/dialog"
+import { signIn } from "next-auth/react"
+
+const SignInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça login na plataforma</DialogTitle>
+        <DialogDescription>
+          Conecte-se usando sua conta do Google.
+        </DialogDescription>
+      </DialogHeader>
+
+      <Button
+        variant="outline"
+        className="gap-1 font-bold"
+        onClick={handleLoginWithGoogleClick}
+      >
+        <Image
+          alt="Fazer login com o Google"
+          src="/google.svg"
+          width={18}
+          height={18}
+        />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SignInDialog
