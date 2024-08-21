bf719a51362395b8b054a220d88a38c7e6ee8b44
Ruan Lins <ruanblins@gmail.com>
2024-08-20 23:05:09 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -45,8 +47,10 @@ const Home = async () => {
       <Header />
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
-        <p>Segunda-feira, 05 de agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session?.user?.name : "bem vindo"}!
+        </h2>
+        <p>{format(new Date(), "E, dd 'de' MMMM", { locale: ptBR })}</p>
 
         {/* BUSCA */}
         <div className="mt-6">
